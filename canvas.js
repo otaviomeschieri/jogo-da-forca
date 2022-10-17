deb6b050a53c5332bf9b94dbf36ac9a54fea211c
Otavio Meschieri <otaviomeschieri@gmail.com>
2022-10-16 22:37:02 -0300

    Alterações no desenho do canvas

--- canvas.js
+++ canvas.js
@@ -3,11 +3,11 @@ function desenharCanvas() {
     tela.lineCap = "round";
     tela.lineJoin = "round";
     tela.strokeStyle = "#0A3871";
-
+    
     //Base da forca
     tela.beginPath();
-    tela.moveTo(100,450);
-    tela.lineTo(450,450);
+    tela.moveTo(100,500);
+    tela.lineTo(450,500);
     tela.stroke();
     tela.closePath();
 }
@@ -18,60 +18,42 @@ function desenharLinhas(index,letra,erros) {
     tela.lineJoin = "round";
     tela.fillStyle = "#F3F5FC";
     tela.strokeStyle = "#0A3871";
-
+    
     let largura = 800/palavraSecreta.length;
     for(let i = 0; i < palavraSecreta.length; i++) {
-        tela.moveTo(250+(largura*i),540);
-        tela.lineTo(300+(largura*i),540);
+        tela.moveTo(500+(largura*i),450);
+        tela.lineTo(550+(largura*i),450);
     }
     escreverLetraCorreta(index);
     escreverLetraIncorreta(letra,erros);
-
+    
     tela.stroke();
     tela.closePath();
 }
-
-// function desenharForca() {
-//     tela.lineWidth = 8;
-//     tela.lineCap = "round";
-//     tela.lineJoin = "round";
-//     tela.fillStyle = "#F3F5FC";
-//     tela.strokeStyle = "#0A3871";
-
-//     tela.beginPath();
-//     tela.moveTo(470,500);
-//     tela.lineTo(470,100);
-//     tela.lineTo(620,100);
-//     tela.lineTo(620,130);
-//     tela.stroke();
-//     tela.closePath();
-// }
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 function desenharForca(erros) {
     tela.lineWidth = 8;
     tela.lineCap = "round";
     tela.lineJoin = "round";
     tela.strokeStyle = "#0A3871";
-
+    
     // Forca
     if(erros === 7) { 
-        tela.moveTo(470,500);
-        tela.lineTo(470,100);
-        tela.moveTo(620,100);
-        tela.lineTo(470,100);
+        tela.moveTo(125,500);
+        tela.lineTo(125,150);
+        tela.moveTo(270,150);
+        tela.lineTo(125,150);
     }
-    // Forca
+    // Corda
     if(erros === 6) { 
-        tela.moveTo(620,100);
-        tela.lineTo(620,130);
+        tela.moveTo(270,150);
+        tela.lineTo(270,180);
     }
     // Cabeça
     if(erros === 5) {
         let emoji02 = new Image();
         emoji02.addEventListener('load',() => {
-            tela.drawImage(emoji02,560,130,78,74);
+            tela.drawImage(emoji02,231,180,78,74);
         });
         emoji02.src = "./imagens/emoji02.png";
     }
@@ -82,122 +64,53 @@ function desenharForca(erros) {
         //     tela.drawImage(emoji03,560,115);
         // },false);
         // emoji03.src = "./imagens/emoji03.png";
-        tela.moveTo(620,222);
-        tela.lineTo(620,320);
+        tela.moveTo(270,254);
+        tela.lineTo(270,370);
     }
     // Perna esquerda
     if(erros === 3) {
-    //     tela.clearRect(560,115,690,233);
-    //     let emoji04 = new Image();
-    //     emoji04.src = "./imagens/emoji04.png";
-    //     emoji04.addEventListener('load',() => {
-    //         tela.drawImage(emoji04,560,115);
-    // })
-        tela.moveTo(620,322);
-        tela.lineTo(570,383);
+        tela.moveTo(270,370);
+        tela.lineTo(220,431);
     }
     // Perna direita
     if(erros === 2) {
-    //     tela.clearRect(560,115,690,233);
-    //     let emoji05 = new Image();
-    //     emoji05.src = "./imagens/emoji05.png";
-    //     emoji05.addEventListener('load',() => {
-    //         tela.drawImage(emoji05,560,115);
-    // })
-        tela.moveTo(620,322);
-        tela.lineTo(660,383);
+        //     tela.clearRect(560,115,690,233);
+        //     let emoji05 = new Image();
+        //     emoji05.src = "./imagens/emoji05.png";
+        //     emoji05.addEventListener('load',() => {
+        //         tela.drawImage(emoji05,560,115);
+        // })
+        tela.moveTo(270,370);
+        tela.lineTo(320,431);
     }
     // Braço esquerdo
     if(erros === 1) {
-    //     tela.clearRect(560,115,690,233);
-    //     let emoji06 = new Image();
-    //     emoji06.src = "./imagens/emoji06.png";
-    //     emoji06.addEventListener('load',() => {
-    //         tela.drawImage(emoji06,560,115);
-    // })
-        tela.moveTo(620,263);
-        tela.lineTo(570,322);
+        //     tela.clearRect(560,115,690,233);
+        //     let emoji06 = new Image();
+        //     emoji06.src = "./imagens/emoji06.png";
+        //     emoji06.addEventListener('load',() => {
+        //         tela.drawImage(emoji06,560,115);
+        // })
+        tela.moveTo(270,290);
+        tela.lineTo(230,330);
     }
     // Braço direito
     if(erros === 0) {
-    //     tela.clearRect(560,115,690,233);
-    //     let cabeca = new Image();
-    //     cabeca.src = "./imagens/emoji07.png";
-    //     cabeca.addEventListener('load',() => {
-    //         tela.drawImage(cabeca,560,115);
-    // })
-        tela.moveTo(620,263);
-        tela.lineTo(660,322);
-        // document.getElementById("perdeu").style.display = "flex";
-        // document.getElementById("mostra-palavra-secreta").innerHTML = "A palavra era " + palavraSecreta;
+        //     tela.clearRect(560,115,690,233);
+        //     let cabeca = new Image();
+        //     cabeca.src = "./imagens/emoji07.png";
+        //     cabeca.addEventListener('load',() => {
+        //         tela.drawImage(cabeca,560,115);
+        // })
+        tela.moveTo(270,290);
+        tela.lineTo(310,330);
+        tela.stroke();
+        tela.closePath();
         exibirDerrota();
     }
-    tela.stroke()
-    tela.closePath()
-  }
-
-  
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-// function desenharCabeca() {
-//     let cabeca = new Image();
-//     cabeca.src = "./imagens/emoji02.png";
-//     cabeca.addEventListener('load',() => {
-//         tela.drawImage(cabeca,560,115)
-//     })
-// }
-
-// function desenharTronco() {
-//     let tronco = new Image();
-//     tronco.src = "./imagens/tronco.png";
-//     tronco.addEventListener('load',() => {
-//         tela.drawImage(tronco,50,50)
-//     })
-// }
-
-// function desenharBracoEsquerdo() {
-//     let bracoEsquerdo = new Image();
-//     bracoEsquerdo.src = "./imagens/braco-esquerdo.png";
-//     bracoEsquerdo.addEventListener('load',() => {
-//         tela.drawImage(bracoEsquerdo,60,60)
-//     })
-// }
-
-// function desenharBracoDireito() {
-//     let bracoDireito = new Image();
-//     bracoDireito.src = "./imagens/braco-direito.png";
-//     bracoDireito.addEventListener('load',() => {
-//         tela.drawImage(bracoDireito,80,80)
-//     })
-// }
-
-// function desenharPernaEsquerda() {
-//     let pernaEsquerda = new Image();
-//     pernaEsquerda.src = "./imagens/perna-esquerda.png";
-//     pernaEsquerda.addEventListener('load',() => {
-//         tela.drawImage(pernaEsquerda,100,100)
-//     })
-// }
-
-// function desenharPernaDireita() {
-//     let pernaDireita = new Image();
-//     pernaDireita.src = "./imagens/perna-direita.png";
-//     pernaDireita.addEventListener('load',() => {
-//         tela.drawImage(pernaDireita,40,40)
-//     })
-// }
-
-//Alterar com mensagem de FIM DE JOGO
-/*function fimDeJogo() {
-        tela.fillStyle = "black";
-		tela.beginPath();
-		tela.moveTo(50, 50);
-		tela.lineTo(50, 400);
-		tela.lineTo(400, 400);
-		tela.fill();
-	}*/
-
+    tela.stroke();
+    tela.closePath();
+}
 
 function nomeDaCategoria(index) {
     tela.font = "bold 64px Source Code Pro";
@@ -205,7 +118,7 @@ function nomeDaCategoria(index) {
     tela.fillStyle = "#000000";
     tela.lineWidth = 6;
     tela.textAlign = "center";
-
+    
     let categoria = index;
     
     if(categoria < 5) {
@@ -223,7 +136,7 @@ function escreverLetraCorreta(index) {
     tela.fillStyle = "#0A3871";
     tela.lineWidth = 6;
     let largura = 800/palavraSecreta.length;
-    tela.fillText(palavraSecreta[index],273 + (largura * index),540);
+    tela.fillText(palavraSecreta[index],523 + (largura * index),450);
 }
 
 function escreverLetraIncorreta(letra,erros) {
@@ -231,7 +144,7 @@ function escreverLetraIncorreta(letra,erros) {
     tela.lineCap = "round";
     tela.fillStyle = "#0A3871";
     tela.lineWidth = 6;
-    tela.fillText(letra,270 + (40 * (10 - erros)),720,40);
+    tela.fillText(letra,520 + (40 * (10 - erros)),570,40);
     // desenharErros();
     desenharForca(erros);
 }
@@ -282,7 +195,7 @@ function exibirVitoria() {
     // if(acertos == palavraSecreta.length) {
     //     letras = [];
     //     tela.clearRect(0,0,1300,860);
-        
+    
     // }
     tela.fill = "red";
     tela.font = "bold 42px Source Code Pro";
@@ -292,10 +205,10 @@ function exibirVitoria() {
     tela.fillStyle="green";
     tela.fillText("Ganhou,",950,320);
     tela.fillText("Parabéns!",930,360);
-
-
-
-
-
+    
+    
+    
+    
+    
     // setTimeout( recarregar , 1000);
 }
--- forca.js
+++ forca.js
@@ -66,25 +66,26 @@ function verificarLetraClicada(key) {
 function xis(letra) {
   // if(!letras.includes(letra)) {
   //     letras.push(letra)
-      if(palavraSecreta.includes(letra)) {
-        adicionarLetraCorreta(palavraSecreta.indexOf(letra))
-          for(let i = 0; i < palavraSecreta.length; i++) {
-              if(palavraSecreta[i] === letra) {
-                  escreverLetraCorreta(i);
-                  document.getElementById(letra).style.background = "green"
-                  verificarVencedor(letra);
-              }
-          }
-      } else {
-          // adicionarLetraIncorreta();
-          document.getElementById(letra).style.background = "red"
-          desenharForca(erros)
-          verificarFimDeJogo(letra)
-          
+  document.getElementById(letra).style.transitionDuration = "0.5s";
+  document.getElementById(letra).style.color = "white";
+  document.getElementById(letra).style.background = "gray";
+  if(palavraSecreta.includes(letra)) {
+    adicionarLetraCorreta(palavraSecreta.indexOf(letra))
+    for(let i = 0; i < palavraSecreta.length; i++) {
+      if(palavraSecreta[i] === letra) {
+        escreverLetraCorreta(i);
+        // document.getElementById(letra).style.background = "green"
+        verificarVencedor(letra);
       }
-      console.log(letras)
+    }
+  } else {
+    // adicionarLetraIncorreta();
+    // document.getElementById(letra).style.background = "red"
+    desenharForca(erros)
+    verificarFimDeJogo(letra)    
+  }
+  console.log(letras)
   // }
-
 }
 
 function adicionarLetraCorreta(i) {
@@ -99,7 +100,7 @@ function adicionarLetraIncorreta(letter) {
 
 function verificarFimDeJogo(letra) {
   //checa se a letra já foi incluída no array de letras certas ou erradas
- if(letraEscolhida.length < palavraSecreta.length) { 
+  if(letraEscolhida.length < palavraSecreta.length) { 
     //incluindo as letras já digitadas no array
     letrasIncorretas.push(letra);
     
@@ -114,7 +115,7 @@ function verificarFimDeJogo(letra) {
       letras = []
     }
   }
- } 
+} 
 
 //Verifica se o usuário ganhou
 function verificarVencedor(letra) {
@@ -146,21 +147,21 @@ function salvarPalavra() {
   
   //captura o que foi digitado
   let novaPalavra = document.getElementById("input-nova-palavra").value;
-
+  
   // let quantidadeMinima = document.getElementById("input-nova-palavra").minLength;
-
+  
   // Alerta de quantidade mínima
   // if(quantidadeMinima > document.getElementById("input-nova-palavra").innerHTML) {
   //   alert("A palavra deve ter no mínimo 4 caracteres");
   // } else {
   //   alert("Ok");
   // }
-
+  
   // inclui a palavra digitada no array de palavras a serem sorteadas
   if(novaPalavra !== ""){
     sorteiaTodas.push(novaPalavra.toUpperCase());
     alert("A palavra digitada foi salva");
-  
+    
     // faz a tela de adicionar palavra desaparecer
     document.getElementById("adicionar-palavra").style.display = "none";
     iniciarJogo(sorteiaTodas,5)
@@ -172,25 +173,25 @@ function salvarPalavra() {
 
 //inicia o jogo
 function iniciarJogo(sorteia,index) {
-
+  
   // faz com que os botões da tela home desapareçam
   document.getElementById("div-titulo").style.display = "none";
   document.getElementById("div-desaparece").style.display = "none";
-
+  
   //chama a função que desenha o canva
   desenharCanvas();
-
+  
   //chama a função que sorteia a palavra 
   escolherPalavraSecreta(sorteia);
-
+  
   //chama a funão que desenha as linhas
   desenharLinhas();
-
+  
   nomeDaCategoria(index);
-
+  
   // document.getElementById("teclado-virtual").style.display = "flex";
   mostrarTeclado();
-
+  
   // faz com que os botões de "novo jogo" e "sair" apareceçam
   document.getElementById("btn-novo-jogo").style.display = "block";
   
@@ -198,9 +199,9 @@ function iniciarJogo(sorteia,index) {
   document.onkeydown = (e) => {
     // coloca a letra digitada em maiúscula
     let letra = e.key.toUpperCase();
-
+    
     verificarLetraClicada(letra)
-
+    
     //verifica se o usuário não perdeu
     if (letrasIncorretas.length <= numeroDeErros) {
       if (!verificarLetraClicada(e.key) && verificarLetra(e.keyCode)) {
@@ -219,7 +220,7 @@ function clicaLetra(e) {
   document.getElementById(e).onclick = () => {
     xis(tecla);
   }
-
+  
   let tecla = e
   console.log(tecla)
   xis(tecla)
